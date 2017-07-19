394a766ebd2f66767897c0bf40f09c0466d10ef3
kura <kura@ino.local>
2017-07-19 23:36:41 +0900

    add snapshot test for components

--- src/reducers/main.test.js
+++ src/reducers/main.test.js
@@ -2,6 +2,9 @@ import main from './main.js';
 
 describe('spec for Reducer', function() {
   const initialState = { count: 0 };
+  it('DEFAULT', () => {
+    expect(main(initialState, { type: 'DEFAULT' })).toEqual({ count: 0 });
+  });
   it('INC', () => {
     expect(main(initialState, { type: 'INC' })).toEqual({ count: 1 });
   });
